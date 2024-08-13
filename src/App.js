6795206cac23d95f48edfbab0cb1d482976b695c
Borislav Gizdov <borislav.gizdov@gmail.com>
2024-08-13 22:42:11 +0300

    Styled with tailwind and flowbite

--- src/App.js
+++ src/App.js
@@ -57,10 +57,11 @@ function App() {
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleCleareComplete}>Clear Complete</button>
       <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+      <input ref={todoNameRef} type="text" id="todo" class="mt-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write todo item" />
+      <button onClick={handleAddTodo} type="button" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Todo</button>
+      <button onClick={handleCleareComplete} type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Clear Complete</button>
+      
     </>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -6,9 +6,9 @@ export default function Todo({ todo, toggleTodo }) {
     }
 
     return (
-        <div>
+        <div className='pl-2 w-64 mt-1 block bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
             <label>
-                <input type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
+                <input className="mr-2 mt-2 mb-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" checked={todo.complete} onChange={handleTodoClick} />
                 {todo.name}
             </label>
         </div>
